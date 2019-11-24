f8169a9b421ad017c39954b434002804836db80e
Shubham Rindhe <shubhamrindhe00@gmail.com>
2019-11-24 19:22:34 +0530

    darkmode

---
+++ scripts/AJAX.js
@@ -0,0 +1,28 @@
+function request(url,callback,Async=true,method="GET",data=null){
+	var xhttp = new XMLHttpRequest();
+	xhttp.overrideMimeType("text/plain");
+	//xhttp.overrideMimeType("text/json");
+	//xhttp.overrideMimeType("application/json");
+	
+	xhttp.onreadystatechange = function(e) {
+		if(this.readyState == 4 && this.status == 200){
+			callback(xhttp.responseText);
+		}
+	};
+	xhttp.open(method,url,Async);
+	
+	//xhttp.responseType = "text";
+	
+	xhttp.send(data);
+	
+	//Asynchronous
+}
+
+	
+function loadJSON(url){
+	let JSobject;
+	request(url,function(response){ 
+		JSobject = JSON.parse(response);
+	},false,"POST",null);
+	return JSobject;
+}
---
+++ scripts/api.github.js
@@ -0,0 +1,5 @@
+const USR = "shubhamrindhe";
+var url = {
+	user : "https://api.github.com/users/"+USR+"",
+	repos : "https://api.github.com/users/"+USR+"/repos"
+}
