2d6cdb20be5ae43367139806bca913335a3d745f
Shubham Rindhe <shubhamrindhe00@gmail.com>
2019-11-27 01:23:15 +0530

    introducing #ajax_indicator

--- scripts/api.github.js
+++ scripts/api.github.js
@@ -3,3 +3,21 @@ var url = {
 	user : "https://api.github.com/users/"+USR+"",
 	repos : "https://api.github.com/users/"+USR+"/repos"
 }
+
+var handlers = {
+	
+	repos : function(evt,obj,res){
+		document.getElementById('container').innerHTML = res;
+	}
+	
+}
+
+function handler(evt,obj){
+	//console.log(evt,obj);
+	document.getElementById('container').innerHTML = '';
+	document.getElementById('ajax_indicator').classList.toggle('active');
+	request(url[obj.id],function(res){
+		handlers[obj.id](evt,obj,res);
+		document.getElementById('ajax_indicator').classList.toggle('active');
+	});
+}
