d8840066197d30e6316209a85ab07d08c92d3012
shubhamrindhe <rindheshubham@gmail.com>
2022-02-05 22:24:13 +0530

    mux-importer

--- importer.js
+++ importer.js
@@ -1,44 +1,54 @@
 
+const MODULES_CACHE = {};
+
 export default class Importer {
 
 	constructor(root) {
 		this.root = root;
 		this.modulePaths = null;
 		this.modules = {};
-
-		this.importPathData();
 		this.finalCallback = null;
-
 		this.modulesToImport = null;
 		this.importedModules = null;
 		this.importHandlers = null;
 		this.errorHandler = null;
+		this.subscribers = [];
 	}
 
-	async importPathData() {
-		import(this.exportPath).then(module => {
-			this.modulePaths = module.default;
+	get exportPath() { return `${this.basePath}/export.js`; }
+	get basePath() { return `./${this.root}`; }
+	get allFetched() { return this.modulesToImport.size == this.importedModules.size; }
 
-			if (this.modulesToImport != null) {
-				this.import(this.moduleKeyHandlersPair, this.finalCallback);
-			}
-		}).catch(error => {
-			console.error(new Error(`export.js file not found in ${this.root}`), error);
-		});
+	addSubscriber(subscriber) {
+		this.subscribers.push(subscriber);
 	}
 
-	get exportPath() {
-		return `${this.basePath}/export.js`;
+	static seek(rootKey, moduleKey) {
+		if (rootKey in MODULES_CACHE) {
+			const root = MODULES_CACHE[rootKey];
+			if (moduleKey in root) return root[moduleKey];
+			return root
+		}
+		return null;
 	}
 
-	get basePath() {
-		return `./${this.root}`;
+	static cache(rootKey, moduleKey, module) {
+		if (!MODULES_CACHE[rootKey]) MODULES_CACHE[rootKey] = {};
+		MODULES_CACHE[rootKey][moduleKey] = module;
 	}
 
-	importModuleWithKey(key) {
+	async importModuleWithKey(key) {
+		const cachedModule = Importer.seek(this.root, key);
+		if (cachedModule) {
+			if (!(key in this.modules)) this.modules[key] = cachedModule;
+			if (!this.importedModules.has(key)) this.importedModules.add(key);
+			this.notifySubscribersModuleImported(key, cachedModule);
+			return;
+		}
+
 		const relativePath = this.modulePaths[key];
 		import(`${this.basePath}${relativePath}`).then(module => {
-			this.handleImportedModule(module, key)
+			this.handleImportedModule(key, module)
 		}).catch(error => {
 			let err = new Error(`file not found in ${this.root}${relativePath}`)
 			console.error(error);
@@ -48,18 +58,30 @@ export default class Importer {
 		});
 	}
 
-	handleImportedModule(module, key) {
+	handleImportedModule(key, module) {
 		this.modules[key] = module;
 		this.importedModules.add(key);
+		Importer.cache(this.root, key, module);
 
+		this.notifySubscribersModuleImported(key, module);
+	}
+
+	notifySubscribersModuleImported(key, module) {
 		if (this.importHandlers) {
 			if (this.importHandlers[key] instanceof Function) {
 				this.importHandlers[key](module);
 			}
 		}
 
-		if (this.modulesToImport.size == this.importedModules.size) {
-			this.finalCallback(this.modules);
+		this.subscribers.forEach((e, i, l) => {
+			if (e.moduleImported instanceof Function) {
+				e.moduleImported(this.root, key, module);
+			}
+		});
+
+		if (this.allFetched) {
+			if (this.finalCallback instanceof Function) this.finalCallback(this.modules);
+			this.subscribers.forEach((e, i, l) => { if (e.allModulesImported instanceof Function) e.allModulesImported(this.root, this.modules); });
 		}
 	}
 
@@ -78,12 +100,98 @@ export default class Importer {
 	}
 
 	async import(moduleData, finalCallback = null) {
-		this.beginImport(moduleData, finalCallback);
+		try {
+			const { default: paths } = await import(this.exportPath);
+			this.modulePaths = paths;
+			this.beginImport(moduleData, finalCallback);
 
-		if (this.modulePaths == null) return;
-
-		this.modulesToImport.forEach(key => {
-			this.importModuleWithKey(key);
-		});
+			this.modulesToImport.forEach(key => {
+				this.importModuleWithKey(key);
+			});
+		} catch (error) {
+			console.log(error);
+		}
 	}
-}
+};
+
+export class MUX {
+
+	constructor(...importers) {
+		this.importers = importers;
+		this.importerKeySet = new Set();
+		this.importedSet = new Set();
+		this.importers.forEach((importer, i, l) => {
+			importer.addSubscriber(this);
+			this.importerKeySet.add(importer.root);
+		});
+
+		this.moduleImportHandler = null;
+		this.completionHandler = null;
+	}
+
+	add(importer) {
+		if (this.importerKeySet.has(importer.root)) return;
+		importer.addSubscriber(this);
+		this.importers.push(importer);
+		this.importerKeySet.add(importer.root);
+	}
+
+	get allFetched() { return this.importerKeySet.size == this.importedSet.size; }
+	get modules() {
+		let _modules_ = {};
+		this.importers.forEach((importer) => { _modules_[importer.root] = importer.modules; });
+		return _modules_;
+	}
+
+	moduleImported(rootKey, moduleKey, module) {
+		const moduleImportData = { rootKey, moduleKey, module };
+		if (this.moduleImportHandler instanceof Function) this.moduleImportHandler(moduleImportData);
+	}
+
+	allModulesImported(rootKey, modules) {
+		this.importedSet.add(rootKey);
+		if (this.allFetched && this.completionHandler instanceof Function) this.completionHandler(this.modules);
+	}
+};
+
+export class MUXEDImporter {
+
+	constructor(...rootKeys) {
+		this.rootKeys = rootKeys;
+		this.map = new Map();
+		this.mux = null;
+		this.setupMUX();
+	}
+
+	setupMUX() {
+		let importers = [];
+		this.rootKeys.forEach((rootkey, i, l) => {
+			const importer = new Importer(rootkey);
+			importers.push(importer);
+			this.map.set(rootkey, importer);
+		});
+
+		this.mux = new MUX(...importers);
+	}
+
+	get modules() { this.mux.modules; }
+	set moduleFetchHandler(_) { this.mux.moduleFetchHandler = _; }
+	set completionHandler(_) { this.mux.completionHandler = _; }
+
+	async importRoot(rootKey, importData, callback) {
+		if (!importData.modules) return;
+		let importer = this.map.get(rootKey);
+
+		if (!(importer instanceof Importer)) {
+			importer = new Importer(rootKey);
+			this.mux.add(importer);
+		}
+
+		importer.import(importData.modules, importData.callback);
+	}
+
+	async import(importData, callback) {
+		for (const rootKey in importData) this.importRoot(rootKey, importData[rootKey]);
+		if (callback instanceof Function) this.completionHandler = callback;
+	}
+};
